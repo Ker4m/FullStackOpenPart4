3d04f3c41637d0eea66aee937c0b44fef84a3d83
Marek Elmayan <elmayan.marek@gmail.com>
2022-12-02 15:09:33 +0200

    Ex 4.3

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-require("dotenv").config();
-const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
+app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+    response.json(blogs)
+  })
+})
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
   blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+    response.status(201).json(result)
+  })
+})
 
-const PORT = 3003;
+const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
