2b1b14240b52c268026c6f8aee87c41d081d3d92
Marek Elmayan <elmayan.marek@gmail.com>
2022-12-02 15:25:18 +0200

    Ex 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.forEach((b) => {sum += b.likes})
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
