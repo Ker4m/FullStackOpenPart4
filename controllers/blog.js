628d2b47c571eedd31b35e7610e9d4c544e3abb0
Marek Elmayan <elmayan.marek@gmail.com>
2022-12-03 16:57:55 +0200

    Ex 4.17

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,15 +1,25 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+
+  // const user = await User.findById(request.body.userId)
+  const user = await User.findOne({})
+
+  blog.user = user._id
+
   if(!blog.title && !blog.url)response.status(400)
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 usersRouter.get('/', async (request, response) => {
   const users = await User
     .find({})
-    // .populate('notes', { content: 1, date: 1 })
+    .populate('blogs', { url: 1, title: 1, author: 1 })
 
   response.json(users)
 })
